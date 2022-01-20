0d3cc45c80b5ad68fa01b2b1e4cf645d67495802
Rory Hendrickson <hendricksonrory@gmail.com>
2022-01-20 11:05:49 -0800

    root relative fetch

--- src/stores.js
+++ src/stores.js
@@ -1,16 +1,21 @@
-import { readable } from 'svelte/store';
+import { readable } from "svelte/store";
 
 export const icons = readable([], async (set) => {
-    const res = await fetch(`https://lucid-heyrovsky-a6ebcb.netlify.app/icons/data.txt`);
-    const text = await res.text();
-    let index = 0;
-    set(text.trim().split("\n").map((line) => {
+  const res = await fetch(`/icons/data.txt`);
+  const text = await res.text();
+  let index = 0;
+  set(
+    text
+      .trim()
+      .split("\n")
+      .map((line) => {
         let dirs = line.split("/");
         return {
           id: index++,
           category: dirs[1],
           iconName: dirs[2],
           iconSet: dirs[3],
-        }
-    }));
+        };
+      })
+  );
 });
